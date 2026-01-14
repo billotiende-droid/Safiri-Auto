cee5af160d406a6a3eba252df606bad2184f161c
billotiende-droid <bill.otiende@student.moringaschool.com>
2026-01-14 21:30:00 +0300

    Import routes and register routes

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from flask_migrate import Migrate
-from models import db, User, Owner, Category, Vehicle, Booking, Payment, Review
+from models import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///safiri.db'
@@ -10,6 +10,29 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 migrate = Migrate(app, db)
 api = Api(app)
+
+# Import routes
+from routes.vehicles import VehicleList, VehicleResource, VehicleStatus
+from routes.bookings import BookingListResource, BookingResource
+from routes.categories import CategoryList
+from routes.payments import Payments, PaymentByID
+
+
+# Register routes
+api.add_resource(VehicleList, '/api/vehicles')
+api.add_resource(VehicleResource, '/api/vehicles/<int:id>')
+api.add_resource(VehicleStatus, '/vehicles/<int:id>/status')
+
+api.add_resource(BookingListResource, "/api/bookings")
+api.add_resource(BookingResource, "/api/bookings/<int:id>")
+
+api.add_resource(Payments,'/api/payments')
+api.add_resource(PaymentByID,'/api/payment/<int:id>')
+
+api.add_resource(CategoryList, '/categories')
+
+
+
 
 # Initialize database (create tables)
 with app.app_context():
