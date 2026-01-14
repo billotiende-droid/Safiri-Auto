e4e4f216dc3612b7c8473b49c1197935d84f0db4
billotiende-droid <bill.otiende@student.moringaschool.com>
2026-01-14 16:20:16 +0300

    config-flask-restful-import-resources

--- server/app.py
+++ server/app.py
@@ -3,6 +3,9 @@ from .config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
+from flask_restful import Api, Resource
+
+
 
 
 db = SQLAlchemy()
@@ -17,14 +20,22 @@ def create_app():
     migrate.init_app(app, db)
     CORS(app)
 
+    api = Api(app)
+
+    #Import resources here to avoid circular imports
+    from .routes.bookings import BookingListResource, BookingResource
+    from .routes.category import CategoryList
+    from .routes.vehicles import VehicleList, VehicleResource, VehicleStatus
+    from .routes.payments import Payments, PaymentByID
+
+
+    # Register resources
+   
+
     # health check route
     @app.route("/health")
     def health():
         return {"status": "ok"}
-
-    #Blueprints will be registered here later
-   
-    # app.register_blueprint(vehicles_bp, url_prefix="/api")
 
     return app
 
