b3a46b49926427fa32361f6d0d021ff58a17f335
Mr-Dan-kibet <kibetdan56@gmail.com>
2026-01-14 16:27:02 +0300

    imports models correctly

--- routes/payments.py
+++ routes/payments.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource
 from flask import request
-from server import db, Payment, Booking
+from server.models import db, Payment, Booking
 from datetime import datetime
 import random
 import string
