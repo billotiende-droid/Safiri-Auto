44569c252df7df450d6c4dbc72d7617dbdfdd57a
john.barsigo@student.moringaschool.com <john.barsigo@student.moringaschool.com>
2026-01-14 10:11:24 +0300

    Added Vehicle and Category Routes. Vehicle.py and Category.py modified

---
+++ routes/categories.py
@@ -0,0 +1,31 @@
+
+from flask import request, jsonify
+from flask_restful import Resource
+from server.models import db, Category
+
+
+# GET and POST for Categories
+class CategoryList(Resource):
+
+    # GET
+    def get(self):
+        categories = Category.query.all()
+        return [c.to_dict() for c in categories], 200
+
+    # POST
+    def post(self):
+        data = request.get_json()
+
+        if not data or not data.get('name'):
+            return {"error": "Category name is required"}, 400
+
+        if Category.query.filter_by(name=data['name']).first():
+            return {"error": "Category already exists"}, 409
+
+        category = Category(name=data['name'])
+        db.session.add(category)
+        db.session.commit()
+
+        return category.to_dict(), 201
+
+api.add_resource(CategoryList, '/categories')
