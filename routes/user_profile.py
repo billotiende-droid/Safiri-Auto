2f22a3765f7a403345aead23e60b440c14dac208
john.barsigo@student.moringaschool.com <john.barsigo@student.moringaschool.com>
2026-01-15 18:57:19 +0300

    Modified user_profile.py

--- routes/user_profile.py
+++ routes/user_profile.py
@@ -1,6 +1,6 @@
 from services.protected_resource import ProtectedResource
 from flask import request
-from models import User, Owner
+from models import db, User, Owner
 
 
 class UserProfile(ProtectedResource) :
@@ -25,3 +25,28 @@ class UserProfile(ProtectedResource) :
             "phone_number": getattr(account, "phone_number", None),
             "role": role
         }, 200
+    
+    # PUT
+    def put(self):
+
+        user_id = self.user_payload["sub"]
+        role = self.user_payload["role"]
+        data = request.get_json()
+
+        if not data:
+            return {"error":"Request must be JSON"}, 400
+        
+        if role == "user" :
+            account = User.query.get(user_id)
+        else :
+            account = Owner.query.get(user_id)
+        
+        if not account :
+            return {"error":"Account not found."}, 404
+        
+        for field in ["name", "email," "phone_number", "residence"]:
+            if field in data :
+                setattr(account, field, data[field])
+        
+        db.session.commit()
+        return {"message":"Profile updated successfully"}, 200
