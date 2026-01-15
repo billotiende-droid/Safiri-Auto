f1a97a26be0284a1938c2c8a0d2fbc9df0d1c2e4
john.barsigo@student.moringaschool.com <john.barsigo@student.moringaschool.com>
2026-01-15 18:36:31 +0300

    UserProfile resource created

--- routes/user_profile.py
+++ routes/user_profile.py
@@ -1,3 +1,27 @@
 from services.protected_resource import ProtectedResource
 from flask import request
 from models import User, Owner
+
+
+class UserProfile(ProtectedResource) :
+
+    # GET
+    def get(self):
+        user_id = self.user_payload["sub"]
+        role = self.user_payload["role"]
+
+        if role == "user":
+            account = User.query.get(user_id)
+        else :
+            account = Owner.query.get(user_id)
+        
+        if not account :
+            return {"error":"Account not found."}, 404
+        
+        return {
+            "id": account.id,
+            "name": getattr(account, "name", None),
+            "email": account.email,
+            "phone_number": getattr(account, "phone_number", None),
+            "role": role
+        }, 200
