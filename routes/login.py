482f5b164b9e78e88ec149295a80f64ead043371
Mr-Dan-kibet <kibetdan56@gmail.com>
2026-01-15 13:12:46 +0300

    deletes expectations

--- routes/login.py
+++ routes/login.py
@@ -1,12 +1,3 @@
-"""
-EXPECTATIONS
-1. should accept credentials only (email && passwords)
-2.validation fields for; missing fields, empty values and invalid JSON
-3.Authentication(import from auth.py)
-4.return response for successful login
-5.Role baseed access control
-"""
-
 # Imports
 from flask_restful import Resource 
 from models import User, Owner
